ac6aa4759206db52bdf088ef41a9a48449dc64eb
tango8 <ethan.b.tang@gmail.com>
2025-05-28 11:58:25 -0400

    just gonna start the turn based part

---
+++ game/TurnBasedBattler.java
@@ -0,0 +1,24 @@
+public class TurnBasedBattler extends JPanel implements ActionListener, KeyListener {
+  Mobs mc;
+  Mobs enemy;
+
+  public TurnBasedBattler(Mobs mc, Mobs enemy) {
+    this.mc = mc;
+    this.enemy = enemy;
+    
+  }
+
+  public void paintComponent(Graphics abilityPanel)
+    {
+        super.paintComponent(abilityPanel);
+        
+
+
+
+    }
+
+  public void draw(Graphics2D graphics, double headingRadians) {
+
+  }
+
+}
